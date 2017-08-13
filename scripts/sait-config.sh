7eb8418b24ff7e8288b08ed6572a1780b0699d5c
ADSapozhnikov <a.d.sapozhnikov@gmail.com>
2017-08-14 00:13:54 +0300

    Update clean config

--- scripts/sait-config.sh
+++ scripts/sait-config.sh
@@ -2,15 +2,14 @@
 # Setting up projects
 sudo su
 cd /var/www/
-mkdir /var/www/1800option
+mkdir /var/www/MySite
 mkdir /var/www/logs
 
 # Site config
 cp /home/ubuntu/config/web/apache-conf/* /etc/apache2/sites-available/
-cp /home/ubuntu/config/web/wp-configs/1800option-wp-config.php /var/www/1800option/wp-config.php
 
 # Enable sites
-a2ensite 1800option.conf
+a2ensite MySite.conf
 
 # Config project
 chown -R www-data:www-data /var/www/*
